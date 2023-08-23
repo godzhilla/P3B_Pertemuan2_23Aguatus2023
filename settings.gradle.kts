e48d30b1199021071ee31a102e170207d4cbf440
godzhilla <ashilanajlasalsabillawd@mail.ugm.ac.id>
2023-08-23 08:16:54 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "P3B Pertemuan 2 (23 Aguatus 2023)"
+include(":app")
+ 
